ed3d99b0f43c9d5d79d47c9923597dc301cf1264
John Simons <jammsimons@gmail.com>
2024-02-12 23:51:32 +0100

    Moved thunderscope driver internally. Commented out currently as WIP tracing a weird build issue

--- src/thunderscope-driver/PluginInit.cpp
+++
@@ -1,9 +0,0 @@
-#include "../../lib/scopehal/scopehal.h"
-
-#include "SCPILitePCIeTransport.h"
-#include "LiteXOscilloscope.h"
-
-void PluginInit() {
-    AddTransportClass(SCPILitePCIeTransport);
-    AddDriverClass(LiteXOscilloscope);
-}
